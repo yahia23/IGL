42252da3183856e1a3ce77e4646029570d6e219d
hocinekhen <hocinekhen@yahoo.com>
2017-10-25 21:17:47 +0200

    Split method Added

--- IGLTP/IGL/src/igl/StringHelper.java
+++ IGLTP/IGL/src/igl/StringHelper.java
@@ -1,3 +1,40 @@
 public class StringHelper {
-    
+    //this is for spliting the string 
+	   List<String> splitString(String parg,char separ)
+          {
+              List<String> lst = new ArrayList<String>();
+              try
+              {
+                  int i = 0;
+                  String ss = "";
+                  for (i = 0; i <= parg.length(); i++)
+                  {
+                      try
+                      {
+                          if (parg.charAt(i)== separ || i==parg.length())
+                              throw new Exception();
+                          ss += parg.charAt(i);
+  
+                      }
+                      catch (Exception ex)
+                      {
+                          try
+                          {
+                              if(ss!="")
+                              lst.add(ss);
+                          }
+                          catch (Exception e)
+                          {
+                          }
+  
+                          ss = "";
+                      }
+                  }
+              }
+              catch (Exception ex)
+              {
+                System.out.println(ex.getMessage());
+              }
+              return lst;       
+      }
 }
