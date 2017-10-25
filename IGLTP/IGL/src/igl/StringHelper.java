26f48a17a8f099f2724af38e6b1245883ca29921
hocinekhen <hocinekhen@yahoo.com>
2017-10-25 22:49:15 +0200

    add Fractionner method to FractionBranch

--- IGLTP/IGL/src/igl/StringHelper.java
+++ IGLTP/IGL/src/igl/StringHelper.java
@@ -1,3 +1,13 @@
 public class StringHelper {
-    
+// this is for Fractionner method
+      public String Fractionner(String ss,char c)
+    {
+        List<String> lst = splitString(ss, c);
+        String s = "";
+        for (String item : lst)
+        {
+            s += "-" + item + '\n';
+        }
+        return s;
+    }
 }
