7e7041b1a1a186055285742b6a3c0e3e34593ea0
yahia23 <33093049+yahia23@users.noreply.github.com>
2017-10-26 00:00:52 +0100

    Maj + index + SubString

--- IGLTP/IGL/src/igl/StringHelper.java
+++ IGLTP/IGL/src/igl/StringHelper.java
@@ -36,6 +36,47 @@ public class StringHelper {
         }
         
         return s2+s;
-    }    
+    }  
+     public String Maj(String s){
+        String s1 =new String();
+        String s2=new String();
+        ArrayList<String> tab = new ArrayList<>();
+        s=s.toLowerCase();
+         boolean trove = ((s.indexOf(','))!=-1);
+       while(trove){
+           tab.add(s.substring(0, s.indexOf(',')+1));
+           s=s.substring(s.indexOf(',')+1);
+           trove = ((s.indexOf(','))!=-1);
+           
+    }
+        if(s.length()!=-1) tab.add(s);
+        for (int i = 0; i < tab.size(); i++) {
+            s1+=tab.get(i).substring(0,1).toUpperCase()+tab.get(i).substring(1);
+            
+        }
+        return s1;
+}
+         public String SubString(int d,int f, String s){
+        ArrayList<Character> tab = new ArrayList<>();
+        String s1 =new String();
+        for(int i=0;i<s.length();i++){
+            tab.add(s.charAt(i));
+        }
+        
+        for(int i= d ;i<f;i++){
+            s1+=tab.get(i);
+        }
+        return s1;
+    }
+ public int index(String s, char c){
+     ArrayList<Character> tab = new ArrayList<>();
+     for(int i=0;i<s.length();i++){
+         tab.add(s.charAt(i));
+     } 
+     for(int i=0;i<tab.size();i++){
+         if(tab.get(i)==c) return i;
+     }
+     return -1;
+ }
 
 }
