e67c69470a6436ed3b5b452d24c94228c48b7b34
hocinekhen <hocinekhen@yahoo.com>
2017-10-26 01:33:00 +0200

    solved the error of out of range in Join method

--- IGLTP/IGL/src/igl/StringHelper.java
+++ IGLTP/IGL/src/igl/StringHelper.java
@@ -213,7 +213,7 @@ public class StringHelper {
 	// this is for join methode
 	public String  Join (String ss,char c)
     {
-        List<String> lst =new ArrayList<>();//= splitString(ss, ' ');
+        List<String> lst = splitString(ss, ' ');
         for (int i = 0; i < lst.size() - 1; i++)
         {
             String o=lst.get(i);
